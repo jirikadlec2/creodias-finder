3b172d3cee2226c4de9dd8d337af7129b4b9df0f
jirikadlec2 <jirikadlec2@gmail.com>
2020-03-15 19:55:26 +0000

    code refactoring - multiple tiles.

--- creodias_finder/creodias_cmd.py
+++ creodias_finder/creodias_cmd.py
@@ -15,30 +15,29 @@ from creodias_finder import download
 DROPBOX_ACCESS_TOKEN = 'oKFU0w6IHvEAAAAAAAFxV-NhqpjyV4fRJGwBovhIN4kSDj0Ko8YqTDWA71Omrh8D'
 DROPBOX_DIR = '/Apps/mysentinel2'
 
-LOCAL_OUTPUT_DIR = 'C:\\Users\\Admin\\Sentinel-2'
+LOCAL_OUTPUT_DIR = '/tmp/Sentinel-2'
 
 MAX_CLOUD = 70.0
-
+START_DATE = datetime(2016, 1, 1)
+END_DATE = datetime(2016, 12, 31)
 TILES = [
-	{"name": "34UCU", "centroid_lon": 19.4, "centroid_lat": 48.0},
-	{"name": "34UCV", "centroid_lon": 19.4, "centroid_lat": 49.2},
-	{"name": "33UXP", "centroid_lon": 17.0, "centroid_lat": 48.0},
-	{"name": "33UYP", "centroid_lon": 18.3, "centroid_lat": 48.3},
-	{"name": "33UYQ", "centroid_lon": 18.3, "centroid_lat": 49.2},
+{"name": "33UXP", "centroid_lon": 17.1, "centroid_lat": 48.2},
+{"name": "33UYP", "centroid_lon": 18.3, "centroid_lat": 48.3},
+{"name": "33UYQ", "centroid_lon": 18.3, "centroid_lat": 49.2},
 ]
 
 def check_zip_file(zip_filepath):
-	file_without_ext = zip_filepath.stem
-	try:
-		with zipfile.ZipFile(str(zip_filepath)) as zip_file:
-			for member in zip_file.namelist():
-				if file_without_ext in member:
-					return True
-				else:
-					return False
-	except:
-		print('bad zip file {}'.format(str(zip_filepath)))
-		return false
+    file_without_ext = zip_filepath.stem
+    try:
+        with zipfile.ZipFile(str(zip_filepath)) as zip_file:
+            for member in zip_file.namelist():
+                if file_without_ext in member:
+                    return True
+                else:
+                    return False
+    except:
+        print('bad zip file {}'.format(str(zip_filepath)))
+        return false
 
 
 
@@ -81,84 +80,79 @@ def upload(
 
 
 def search_scenes(start_date, end_date, geometry, max_cloud):
-	results = query.query(
-	    'Sentinel2',
-	    start_date=datetime(2016, 1, 1),
-	    end_date=datetime(2016, 12, 31),
-	    geometry=Point(LON, LAT),
-	)
-	filtered_results = []
-	for r in results.values():
-		title = r["properties"]["title"]
-		if title.startswith("S2A_MSIL1C_") or title.startswith("S2A_MSIL1C"):
-			print((title, r["properties"]["cloudCover"]), flush=True)
-			if r["properties"]["cloudCover"] <= max_cloud:
-				filtered_results.append(r)
-	return sorted(filtered_results, key=lambda k: k['properties']['title'])
+    results = query.query(
+        'Sentinel2',
+        start_date=start_date,
+        end_date=end_date,
+        geometry=geometry,
+    )
+    filtered_results = []
+    for r in results.values():
+        title = r["properties"]["title"]
+        if title.startswith("S2A_MSIL1C_") or title.startswith("S2A_MSIL1C"):
+            print((title, r["properties"]["cloudCover"]), flush=True)
+            if r["properties"]["cloudCover"] <= max_cloud:
+                filtered_results.append(r)
+    return sorted(filtered_results, key=lambda k: k['properties']['title'])
 
 
-def download_scene(creodias_scene_uuid, out_file, credentials)
-	downloaded = False
-	while not downloaded:
-		# download single product by product ID
-		try:
-			download.download(r["id"], outfile=out_file, **credentials)
-			if out_file.stat().st_size < 100000:
-				out_file.unlink()
-				print('download incomplete..', flush=True)
-				raise Exception
-			#elif check_zip_file(out_file) == False:
-			#	print('zip file is corrupt.')
-			#	out_file.unlink()
-			#	raise Exception
-			downloaded = True
-			print("{} downloaded successfully.".format(out_file.name), flush=True)
-		except Exception as e:
-			sleep(10)
-			print('download exception {}, retrying ..'.format(str(e)), flush=True)
-			downloaded = False
+def download_scene(creodias_scene_uuid, out_file, credentials):
+    downloaded = False
+    while not downloaded:
+        try:
+            download.download(creodias_scene_uuid, outfile=out_file, **credentials)
+            if out_file.stat().st_size < 100000:
+                out_file.unlink()
+                print('download incomplete..', flush=True)
+                raise Exception
+            #elif check_zip_file(out_file) == False:
+            #   print('zip file is corrupt.')
+            #	out_file.unlink()
+            #	raise Exception
+            downloaded = True
+            print("{} downloaded successfully.".format(out_file.name), flush=True)
+        except Exception as e:
+            sleep(10)
+            print('download exception {}, retrying ..'.format(str(e)), flush=True)
+            downloaded = False
 
 
 def download_scenes_for_tile(tile_centroid_lon, tile_centroid_lat):
 
-	print("creodias_cmd.py", flush=True)
+    print("creodias_cmd.py", flush=True)
 
-	CREDENTIALS = {
-	    'username': 'jiri.kadlec@gisat.cz',
-	    'password': 'Earth6378'
-	}
+    CREDENTIALS = {'username': 'jiri.kadlec@gisat.cz', 'password': 'Earth6378'}
 
-	results = search_scenes(
-		start_date=datetime(2016, 1, 1),
-		end_date = datetime(2016, 12, 31),
-		geometry = Point(tile_centroid_lon, tile_centroid_lat),
-		max_cloud = MAX_CLOUD
-	)
-
-	for r in results:
-		safe_package_name = r["properties"]["title"]
-		tile_id = safe_package_name.split("_")[-2][1:6]
-		creodias_scene_uuid = r["id"]
-		print(tile_id, flush=True)
-		print((safe_package_name, creodias_scene_uuid), flush=True)
-		out_file = Path(LOCAL_OUTPUT_DIR).joinpath(tile_id, safe_package_name + ".zip")
-		if not out_file.parent.is_dir():
-			out_file.parent.mkdir(parents=True, exist_ok=True)
-			print("created output directory: {}".format(out_file.parent))
-		if out_file.is_file():
-			continue
-
-		# Downloads the .SAFE.zip from CreoDias
-		download_scene(creodias_scene_uuid, out_file, CREDENTIALS)
-		# Uploads the downloaded .SAFE.zip to DropBox
-		dropbox_path = '/'.join(DROPBOX_DIR, tile_id, out_file.name)
-		upload(DROPBOX_ACCESS_TOKEN, out_file, dropbox_path)
+    results = search_scenes(
+                start_date=START_DATE,
+                end_date = END_DATE,
+                geometry = Point(tile_centroid_lon, tile_centroid_lat),
+                max_cloud = MAX_CLOUD
+            )
+    for r in results:
+        safe_package_name = r["properties"]["title"]
+        tile_id = safe_package_name.split("_")[-2][1:6]
+        creodias_scene_uuid = r["id"]
+        print(tile_id, flush=True)
+        print((safe_package_name, creodias_scene_uuid), flush=True)
+        out_file = Path(LOCAL_OUTPUT_DIR).joinpath(tile_id, safe_package_name + ".zip")
+        if not out_file.parent.is_dir():
+            out_file.parent.mkdir(parents=True, exist_ok=True)
+            print("created output directory: {}".format(out_file.parent))
+        if out_file.is_file():
+            continue
+        # Downloads the .SAFE.zip from CreoDias
+        download_scene(creodias_scene_uuid, out_file, CREDENTIALS)
+        # Uploads the downloaded .SAFE.zip to DropBox
+        dropbox_path = '/'.join([DROPBOX_DIR, tile_id, out_file.name])
+        upload(DROPBOX_ACCESS_TOKEN, out_file, dropbox_path)
 
 
 def main():
-	for tile in TILES:
-		download_scenes_for_tile(tile['centroid_lon'], tile['centroid_lat'])
+    for tile in TILES:
+        print(tile)
+        download_scenes_for_tile(tile['centroid_lon'], tile['centroid_lat'])
 
 
 if __name__ == "__main__":
-	main()
+    main()
